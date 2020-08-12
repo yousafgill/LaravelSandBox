2d95610e124aba29bab9ede327ffc6f696ba35d1
Yousaf Gill <yousafgill@gmail.com>
2020-08-12 15:45:58 +0500

    minor change

--- saasapp/config/app.php
+++ saasapp/config/app.php
@@ -175,6 +175,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * Other Service Providers
+         */
+          Laravel\Socialite\SocialiteServiceProvider::class,
+
+
     ],
 
     /*
@@ -226,6 +232,13 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+
+        /**
+         * Other Aliases...
+         */
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+
 
     ],
 
